0b4569c8e635c99ce4cdfe2a6df09562d9a367a8
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-04-04 21:00:49 -0400

    New functions for game logic
    Completed working function for checkWinningCombos which tests whether
    the squaresInPlay array has a winning combo. Also completed function for
    updateTurn which changes which player's turn it is when called. Finally,
    began work on make move function. Make move function & game board
    functions are still works in progress @ this point.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,6 +20,48 @@ let squaresInPlay=['', '', '', '', '', '', '', '', '']
 
 let currentTurn = ''
 
+// compare current squaresInPlay array against possible wins or draw
+const checkWinningCombos = function (gamearray) {
+  // check for winning combo 1
+  if ((gamearray[0] === gamearray[1]) && (gamearray[1] === gamearray[2]) && (gamearray[2] !== '')) {
+    let winningPlayer = gamearray[0]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // check for winning combo 2
+  } else if ((gamearray[3] === gamearray[4]) && (gamearray[4] === gamearray[5]) && (gamearray[5] !== '')){
+    let winningPlayer = gamearray[3]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // check for winning combo 3
+  } else if ((gamearray[6] === gamearray[7]) && (gamearray[7] === gamearray[8]) && (gamearray[8] !== '')) {
+    let winningPlayer = gamearray[6]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // check for winning combo 4
+  } else if ((gamearray[0] === gamearray[3]) && (gamearray[3] === gamearray[6]) && (gamearray[6] !== '')) {
+    let winningPlayer = gamearray[0]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // check for winning combo 5
+  } else if ((gamearray[1] === gamearray[4]) && (gamearray[4] === gamearray[7]) && (gamearray[7] !== '')) {
+    let winningPlayer = gamearray[1]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // check for winning combo 6
+  } else if ((gamearray[2] === gamearray[5]) && (gamearray[5] === gamearray[8]) && (gamearray[8] !== '')) {
+    let winningPlayer = gamearray[2]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // check for winning combo 7
+  } else if ((gamearray[0] === gamearray[4]) && (gamearray[4] === gamearray[8]) && (gamearray[8] !== '')) {
+    let winningPlayer = gamearray[0]
+    console.log('Player ' + winningPlayer + ' wins!')
+  } else if ((gamearray[2] === gamearray[4]) && (gamearray[4] === gamearray[6]) && (gamearray[6] !== '')) {
+  let winningPlayer = gamearray[2]
+    console.log('Player ' + winningPlayer + ' wins!')
+    // if all squares filled in with no win combo - notify it's a draw
+  } else if (gamearray.indexOf('') === -1) {
+    console.log('It is a draw! Game Over, no winners. Boo!')
+  // if none of the above - continue playing
+  } else {
+    console.log('keep on playing guys!')
+  }
+}
+
 // Function to create a new board when the 'new game' button is clicked
 const newBoard = function () {
 // set the initial player to be "x"
