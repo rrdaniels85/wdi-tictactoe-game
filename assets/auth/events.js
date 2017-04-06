8c97cff136aae0e6e192f27f5ebf38df98a8304c
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-04-05 20:12:46 -0400

    Basic client side board functionality working
    Updated newBoard function so that it will now clear the board and reset
    the cells array when the new game button is clicked. Updated the
    checkWinningCombos function so that now it logs to the user whether
    there was a win and if there was a draw. It also updates the variable
    over to reflect whether the game is finished.

--- assets/auth/events.js
+++ assets/auth/events.js
@@ -7,7 +7,7 @@ const ui = require('./ui')
 const game = require('../scripts/gamelogic')
 
 let currentTurn = 'x'
-let cells = []
+let cells = ['', '', '', '', '', '', '', '', '']
 let over = false
 
 const onSignUp = function (event) {
@@ -52,33 +52,50 @@ const newBoard = function (event) {
   cells = ['', '', '', '', '', '', '', '', '']
 // set the initial player to be "x"
   currentTurn = 'x'
+// set the over variable to false since game is not over
   over = false
-  console.log(currentTurn + ' begins the game')
+// clear the squares on the board
+  $('.square').text('')
 }
 
+// function to track game play, check for wins and log results
 const clickSquare = function (event) {
+  // prevent page from refreshing
   event.preventDefault()
-  if(over === false){
-  console.log('yep, you clicked that square')
-  const squareId = $(this).attr('id')
-  if ($(this).text()) {
-    console.log('This square has already been played')
-  } else {
-    if (currentTurn === 'x') {
-  // $(this).html("<img src='assets/styles/images/jedi-yoda.jpg'>")
-    $(this).text('X')
-      game.makeMove(currentTurn, squareId, cells, over)
-      currentTurn = 'o'
+  // make sure player can only continue if game is not yet finished
+  if (over === false) {
+    // check to see if the square has already been played
+    if ($(this).text()) {
+      console.log('This square has already been played')
+    // if square has not been played then check to which player made selection
     } else {
-    $(this).text('O')
-      game.makeMove(currentTurn, squareId, cells, over)
-      currentTurn = 'x'
+      // if player x made the selection
+      if (currentTurn === 'x') {
+        // retrieve the id from the clicked element and assign it to squareId
+        const squareId = $(this).attr('id')
+        // mark the square with an 'X'
+        $(this).text('X')
+        // call makeMove function to add move into cells array
+        game.makeMove(currentTurn, squareId, cells)
+        // call checkWinningCombos to see if game is over
+        over = game.checkWinningCombos(cells)
+        // update current turn to the next player
+        currentTurn = 'o'
+        // if player o made selection
+      } else {
+        // retrieve id from the clicked element and assign to squareId
+        const squareId = $(this).attr('id')
+        // mark the square with an 'O'
+        $(this).text('O')
+        // call makeMove function to add move into cells array
+        game.makeMove(currentTurn, squareId, cells, over)
+        // call checkWinningCombos to see if game is over
+        over = game.checkWinningCombos(cells)
+        // update current turn to the next player
+        currentTurn = 'x'
+      }
     }
   }
-  console.log(cells)
-  console.log(currentTurn)
-  console.log(over)
-}
 }
 
 const addHandlers = () => {
